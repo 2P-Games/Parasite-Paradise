4df1c7573837c64fcfcf087111027e9413227a8c
Okma <c.amko@ufl.edu>
2016-02-14 08:41:15 -0500

    Basic Main Menu

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MapManager : MonoBehaviour {
+
+    // generic function for opening a scene
+	public void OpenLevelByString(string levelName)
+    {
+        // main menu
+        if(levelName.Equals("mainmenu", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        // test scene
+        else if(levelName.Equals("testscene", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("TestScene");
+        }
+    }
+
+    // close the game
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
