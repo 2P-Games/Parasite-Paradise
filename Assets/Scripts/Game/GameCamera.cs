d8125ad4fd27398a6d611143a5f1d2cb0ebcccde
Okma <c.amko@ufl.edu>
2016-02-14 09:11:35 -0500

    Added Camera script with player following, renamed _Scenes to Scenes, prototype GameManger class, added TestScene to the build settings, created 3 new layers for future use.

---
+++ Assets/Scripts/Game/GameCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        // GameObject.Find() is typically slow and not recommended, but for a project of our scale I think it's fine.
+        playerReference = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // gameObject is how you reference the object attached to.
+
+        /*
+        * This basically continuously updates the camera's 'x' and 'y' position to always be above the player. 
+        * Note: The X and Y data fields are private and cannot be directly modified in C#, so this a workaround.
+        */
+        gameObject.transform.position = new Vector3(playerReference.transform.position.x, playerReference.transform.position.y, -10);
+
+    }
+
+    private GameObject playerReference;
+}
