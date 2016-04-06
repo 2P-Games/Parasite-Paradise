1a659f6815305ca1416773e4b13a2f85f2d1946f
Jason Ortiz <jayortiz1013@ufl.edu>
2016-04-06 00:23:58 -0400

    Adding MORPH3D Module, Wood Elf Scout model, and Alien Insect Model.

---
+++ Assets/Characters/Scripts/PlayerCtrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	private Animator playerAnimator;
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("q")) {
+			playerAnimator.SetBool ("Withdraw", true);
+			Invoke ("StopWithdraw", 2.0f);
+		}
+	}
+
+	void StopWithdraw() {
+		playerAnimator.SetBool ("Withdraw", false);
+	}
+}
