a41fd5e857e8900935e31bc04919d5f5452d1caa
christian122333 <cargueta@ufl.edu>
2016-04-20 22:46:20 -0400

    Added more robots, changed speed, game ends when player returns to house

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+	private SceneFadeInOut sceneFadeInOut;  
+	private PlayerInventory playerInventory;
+	private Transform player; 
+	// Use this for initialization
+	void Awake() {
+		sceneFadeInOut = GameObject.FindGameObjectWithTag(Tags.fader).GetComponent<SceneFadeInOut>();
+		player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+		playerInventory = player.gameObject.GetComponent<PlayerInventory>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) {
+		if(playerInventory.numberOfParasites >= 3)
+			{
+					sceneFadeInOut.EndScene();
+			}
+	}
+}
